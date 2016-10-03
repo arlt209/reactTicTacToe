59201e90eec43a831f204ed1f7ccf57f96678e38
arlt209 <nickarlt@gmail.com>
2016-10-03 12:46:52 -0700

    Created Board and hover effects.

---
+++ reactTicTactoe/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      PLAYER_ONE_SYMBOL: "x",
+      PLAYER_TWO_SYMBOL: "O",
+      currentTurn: "X",
+      board: [
+        "", "", "", "", "", "", "", "", ""
+      ]
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="board">
+        {this.state.board.map((cell) => {
+          return <div className="square">{cell}</div>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
